42cdb9d59d6c68435f4c6abb9a25e9bf78d91a6c
abhilashmk <abhilashkonnur1@gmail.com>
2020-01-09 15:40:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 
+var port = 0;
 var app = express();
 app.get('/', function (req, res) {
  // res.send('Hello abhilash branch World!');
@@ -13,7 +14,7 @@ res.send("Hello world Linux Abhilash");
  
 });
 
-var port = process.env.PORT || 1337;
+port = process.env.PORT || 1337;
 var server = app.listen(port,function(){
 	
 });
