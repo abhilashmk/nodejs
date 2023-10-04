80fa0808d522559f4c2868c452fd3f994163d19e
abhilashmk <abhilashkonnur1@gmail.com>
2023-10-04 18:51:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
-var sleep = require('sleep');
+const winston = require('winston');
+//var sleep = require('sleep');
+const logConfiguration = {
+    'transports': [
+        new winston.transports.Console()
+    ]
+};
+const logger = winston.createLogger(logConfiguration);
 app.get('/', function (req, res) {
  // res.send('Hello abhilash branch World!');
-sleep.sleep(5)
+//sleep.sleep(5)
 res.send("Hello world Linux Abhilash feature test");
-
 
  
 });
@@ -14,5 +20,5 @@ port = process.env.PORT || 1337;
 var server = app.listen(port,function(){
 	
 });
-
+logger.info('Hello, Winston!');
 console.log("Server running at http://localhost:%d", port);
