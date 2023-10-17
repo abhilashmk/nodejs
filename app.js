8bdbdb30749f94d8b354cedf82f6d36f999b805e
abhilashmk <abhilashkonnur1@gmail.com>
2023-10-17 18:14:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,27 +9,20 @@ const logConfiguration = {
 };
 
 
-const { performance, PerformanceObserver, perf_hooks } = require('perf_hooks');
-
-const obs = new PerformanceObserver((items) => {
-    const entry = items.getEntries()[0];
-    logger.info(`Event Loop Delay: ${entry.duration}ms`);
-});
-
 obs.observe({ entryTypes: ['eventLoopDelay'], buffered: true });
 
 
-//const { monitorEventLoopDelay } = require('perf_hooks');
-//const h = monitorEventLoopDelay({ resolution: 20 });
+const { monitorEventLoopDelay } = require('perf_hooks');
+const h = monitorEventLoopDelay({ resolution: 20 });
 
 const logger = winston.createLogger(logConfiguration);
 app.get('/', function (req, res) {
  // res.send('Hello abhilash branch World!');
-//h.enable();
+h.enable();
 sleep.sleep(5)
 logger.info('Hello, Winston!');
-//h.disable();
-//logger.info(h.max);
+h.disable();
+logger.info(h.max);
 res.send("Hello world Linux Abhilash feature test");
 });
 
